bb5dedb915264fc61ef652237da6106c16cd96af
bobosii <day.emir@hotmail.com>
2025-05-21 00:22:40 +0300

    Initial commit

---
+++ src/main/java/dev/emir/DrivingSchoolWebApp/DrivingSchoolWebAppApplication.java
@@ -0,0 +1,13 @@
+package dev.emir.DrivingSchoolWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingSchoolWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivingSchoolWebAppApplication.class, args);
+	}
+
+}
