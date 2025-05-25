835de4ef313c6854d430868dec6f27826f1ab1fd
bobosii <day.emir@hotmail.com>
2025-05-25 16:24:15 +0300

    initial commit

---
+++ src/test/java/dev/emir/DrivingSchoolWebApp/DrivingSchoolWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emir.DrivingSchoolWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivingSchoolWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
